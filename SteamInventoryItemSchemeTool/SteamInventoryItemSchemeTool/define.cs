b3074065179833a7714d865b3885bf1f1362678a
Evan Wrynn <evan.wrynn@gmail.com>
2018-12-07 16:13:01 +0000

    Create define.cs

---
+++ SteamInventoryItemSchemeTool/SteamInventoryItemSchemeTool/define.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamInventoryItemSchemeTool
+{
+
+
+    public class Rarity
+    {
+        public string Name;
+        public string Color;
+    }
+}
